a1f3a126e9171852d1a7d64d46fb74bec79578aa
OlehStepaniuk <oleh.stepaniuk@chatbots.studio>
2021-03-13 00:37:07 +0200

    init SPA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import { useState } from 'react'
+import { saveAs } from 'file-saver'
+import axios from 'axios';
+
+import './App.css';
+const App = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [conventions, setConventions] = useState('');
+
+  const saveFileHandler = async (values) => {
+    const data = await axios({
+      url: 'http://localhost:5000/generate-stringtable',
+      method: 'POST',
+      data: {
+        title,
+        author,
+        conventions
+      },
+      responseType: 'blob'
+    });
+    saveAs(data.data, 'stringtable.xml')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>MAMaS Stringtable Generator</h1>
+        <span>Создай или редактируй свой Stringtable!</span>
       </header>
+      <main>
+        <section className='main-settings'>
+          <div>
+            <span>Название миссии</span>
+            <input name='title' onChange={(e) => setTitle(e.target.value)} placeholder='Название' />
+          </div>
+          <div>
+            <span>Автор миссии</span>
+            <input name='author' onChange={(e) => setAuthor(e.target.value)} placeholder='Автор' />
+          </div>
+          <div>
+            <span>Условности миссии</span>
+            <textarea name='conventions' onChange={(e) => setConventions(e.target.value)} placeholder='Условности' width='250px' />
+          </div>
+        </section>
+
+        <section className='sides'>
+          <div className='bluefor'>
+            <div>
+              <span>Описание ситуации | BLUEFOR</span>
+              <input name='conventions' onChange={(e) => setConventions(e.target.value)} placeholder='Ситуация ()' />
+            </div>
+          </div>
+          <div className='redfor'>
+            <div>
+              <span>Условности миссии</span>
+              <input name='conventions' onChange={(e) => setConventions(e.target.value)} placeholder='Условности' />
+            </div>
+          </div>
+          <div className='independent'>
+
+          </div>
+        </section>
+        <button onClick={saveFileHandler}>Click!</button>
+      </main>
+      <footer>
+        Created by Agentos. <br />Idea and host by XDred.
+      </footer>
     </div>
   );
 }
